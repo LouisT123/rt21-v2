f9e3b327d34452351c30339223d9bdcf5acfc6b3
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-05-08 14:08:00 -0700

    Looping command

--- rt_21_attempt5/rt_21_attempt5.cpp
+++ rt_21_attempt5/rt_21_attempt5.cpp
@@ -1,74 +1,92 @@
-#include <windows.h>
 #include <iostream>
+#include <Windows.h>
+#include <string>
 
 int main()
 {
     HANDLE hSerial;
     DCB dcbSerialParams = { 0 };
     COMMTIMEOUTS timeouts = { 0 };
-    wchar_t comP[] = L"COM3";
+    wchar_t comport[] = L"COM3"; //change this com port to whatever 
 
     // Open the serial port
-    hSerial = CreateFile(comP, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    hSerial = CreateFile(comport, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
     if (hSerial == INVALID_HANDLE_VALUE)
     {
-        std::cerr << "Error opening serial port\n";
+        std::cout << "Error opening serial port." << std::endl;
         return 1;
     }
 
-    // Set the serial port parameters
+    // Set serial port parameters
     dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
     if (!GetCommState(hSerial, &dcbSerialParams))
     {
-        std::cerr << "Error getting serial port state\n";
+        std::cout << "Error getting serial port state." << std::endl;
         CloseHandle(hSerial);
         return 1;
     }
 
-    dcbSerialParams.BaudRate = 4800; // Set the baud rate to 9600
-    dcbSerialParams.ByteSize = 8; // 8 data bits
-    dcbSerialParams.StopBits = ONESTOPBIT; // 1 stop bit
-    dcbSerialParams.Parity = NOPARITY; // no parity
+    dcbSerialParams.BaudRate = CBR_4800;  // Change baud rate if necessary, but shoudl be 4800 8N1 for rt21 dcu-1 protocol
+    dcbSerialParams.ByteSize = 8;         // Number of bits/byte, 8
+    dcbSerialParams.Parity = NOPARITY;    // No parity
+    dcbSerialParams.StopBits = ONESTOPBIT;// One stop bit
+
     if (!SetCommState(hSerial, &dcbSerialParams))
     {
-        std::cerr << "Error setting serial port state\n";
+        std::cout << "Error setting serial port state." << std::endl;
         CloseHandle(hSerial);
         return 1;
     }
 
     // Set timeouts
-    timeouts.ReadIntervalTimeout = 50;
-    timeouts.ReadTotalTimeoutConstant = 50;
-    timeouts.ReadTotalTimeoutMultiplier = 10;
-    timeouts.WriteTotalTimeoutConstant = 50;
-    timeouts.WriteTotalTimeoutMultiplier = 10;
+    timeouts.ReadIntervalTimeout = 50;           // Maximum time between read operations
+    timeouts.ReadTotalTimeoutConstant = 50;      // Constant timeout for reading
+    timeouts.ReadTotalTimeoutMultiplier = 10;    // Multiplier timeout for reading
+    timeouts.WriteTotalTimeoutConstant = 50;     // Constant timeout for writing
+    timeouts.WriteTotalTimeoutMultiplier = 10;   // Multiplier timeout for writing
+
     if (!SetCommTimeouts(hSerial, &timeouts))
     {
-        std::cerr << "Error setting timeouts\n";
+        std::cout << "Error setting timeouts." << std::endl;
         CloseHandle(hSerial);
         return 1;
     }
 
-    // Read and write data
-    char data[] = "AIn;";
-    DWORD bytes_written, bytes_read;
-    if (!WriteFile(hSerial, data, sizeof(data), &bytes_written, NULL))
+    // Loop until the user enters "stop"
+    std::string userInput;
+    while (userInput != "stop")
     {
-        std::cerr << "Error writing to serial port\n";
-        CloseHandle(hSerial);
-        return 1;
-    }
-    Sleep(500);
-    char buf[140];
-    if (!ReadFile(hSerial, buf, sizeof(buf), &bytes_read, NULL))
-    {
-        std::cerr << "Error reading from serial port\n";
-        CloseHandle(hSerial);
-        return 1;
-    }
+        // Ask for user input
+        std::cout << "Enter text (or 'stop' to exit): ";
+        std::getline(std::cin, userInput);
 
-    std::cout << "Received data: " << buf << std::endl;
+        // Write data to the serial port
+        DWORD bytesWritten;
+        if (!WriteFile(hSerial, userInput.c_str(), userInput.length(), &bytesWritten, NULL))
+        {
+            std::cout << "Error writing to serial port." << std::endl;
+            CloseHandle(hSerial);
+            return 1;
+        }
+
+        std::cout << "Bytes written: " << bytesWritten << std::endl;
+
+        Sleep(500);
+
+        // Read data from the serial port
+        char buffer[100];
+        DWORD bytesRead;
+        if (!ReadFile(hSerial, buffer, sizeof(buffer), &bytesRead, NULL))
+        {
+            std::cout << "Error reading from serial port." << std::endl;
+            CloseHandle(hSerial);
+            return 1;
+        }
+
+        std::cout << "Bytes read: " << bytesRead << std::endl;
+        std::cout << "Data received: " << buffer << std::endl;
+    }
 
     // Close the serial port
     CloseHandle(hSerial);
